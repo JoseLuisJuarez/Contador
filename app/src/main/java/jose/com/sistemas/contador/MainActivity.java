f9d31f373e048501b0925f72dfe7296abe2bda4e
Jose Luis Juarez Rafael <jose210795@gmail.com>
2017-02-23 15:41:26 -0500

    Initial Commit

---
+++ app/src/main/java/jose/com/sistemas/contador/MainActivity.java
@@ -0,0 +1,74 @@
+package jose.com.sistemas.contador;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    public int contador;
+
+    TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textoResultado=(TextView)findViewById(R.id.contadorTexto);
+        contador=0;
+
+    }
+    //crear métodos
+
+    public void incrementaContador(View vista){
+
+        contador++;
+
+        textoResultado.setText("" + contador);
+
+    }
+
+    public void restaContador(View vista){
+
+        contador--;
+
+        if (contador<0){
+
+            CheckBox negativos=(CheckBox)findViewById(R.id.negativos);
+
+            if (!negativos.isChecked()){
+
+                contador=0;
+
+            }
+
+        }
+            textoResultado.setText(""+contador);
+    }
+
+    public void reseteaContador(View vista){
+
+
+        EditText numero_reset=(EditText)findViewById(R.id.n_reseteo);
+
+        try{
+        contador=Integer.parseInt(numero_reset.getText().toString());
+        }catch(Exception e){
+            contador=0;
+        }
+        numero_reset.setText("");
+
+        textoResultado.setText("" + contador);
+    }
+
+    /*public void mostrarResultado(){
+
+        TextView textoResultado=(TextView) findViewById(R.id.contadorTexto);
+
+        textoResultado.setText("" + contador);
+    }*/
+
+}
